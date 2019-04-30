e19e1575762597df5fa77b6aded600c398041210
Yijun Blue Yuan <Blue.Yuan@morningstar.com>
2019-04-30 14:10:34 -0500

    remove wrapper

--- emacs.bat
+++
@@ -1,7 +0,0 @@
-set HOME=%UserProfile%
-set PATH=%PATH%;%ProgramFiles(Opt)%\texlive\2013\bin\win32;
-set EXE=runemacs.exe
-set BIN_FOLDER=%ProgramFiles(Opt)%\emacs-26.2-x64\bin
-set EMACS=%BIN_FOLDER%\%EXE%
-cd "%HOME%"
-"%EMACS%" --load=%HOME%\.emacs.d\init.el %1
