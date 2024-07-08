00dd11333f91879d014b6b450c244a67886eb8d3
Yijun Blue Yuan <Blue.Yuan@morningstar.com>
2024-07-08 14:15:10 -0500

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(ns-set-resource nil "ApplePressAndHoldEnabled" "NO")
 (package-initialize)
 
 (require 'server)
@@ -20,3 +21,13 @@
 
 (display-time-mode 1)
 
+
+;; OSX compatible bindings
+(when (memq window-system '(mac ns x))
+  (global-set-key [home] 'beginning-of-line-text)
+  (global-set-key [end] 'move-end-of-line)
+  (global-set-key (kbd "<M-right>") 'right-word)
+  (global-set-key (kbd "<M-left>") 'left-word)
+  (global-set-key (kbd "<M-up>") nil) ; 'backward-up-list)
+  (global-set-key (kbd "<M-down>") nil) ; 'forward-list)
+  (global-set-key (kbd "<M-delete>") 'kill-word))
